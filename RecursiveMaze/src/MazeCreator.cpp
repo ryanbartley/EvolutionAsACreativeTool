4a64abe9267f6850aa48b117dddef83acbad3540
ryanbartley <ryanbartley@gmail.com>
2013-10-01 15:39:43 -0400

    semi working maze generator

--- RecursiveMaze/src/MazeCreator.cpp
+++ RecursiveMaze/src/MazeCreator.cpp
@@ -7,3 +7,98 @@
 //
 
 #include "MazeCreator.h"
+
+void MazeCreator::generate()
+{
+	killArray();
+	maze = new BYTE[sideCellAmount * sideCellAmount];
+	memset(maze, 0, sideCellAmount * sideCellAmount);
+	currentX = rand() % sideCellAmount; currentY = rand() % sideCellAmount;
+}
+
+std::vector<ci::PolyLine2f> MazeCreator::carve()
+{
+	std::vector<ci::PolyLine2f> mazeLine;
+	ci::PolyLine2f singleLine;
+	int previousX = 0, previousY = 0;
+	while( true )
+	{
+		if( (currentX > previousX + 2) || (currentY > previousY + 2) ) {
+			mazeLine.push_back(singleLine);
+			singleLine.getPoints().clear();
+		}
+		else {
+			previousX = currentX;
+			previousY = currentY;
+		}
+		int d = getDirection();
+		if( d < NOR ) return mazeLine;
+		
+		int nx = currentX * CELL_SIZE,
+		ny = currentY * CELL_SIZE;
+		
+		switch( d )
+		{
+			case NOR:
+				maze[currentX + sideCellAmount * currentY] |= NOR; currentY--;
+				maze[currentX + sideCellAmount * currentY] = SOU | SOU << 4;
+				singleLine.push_back( ci::Vec2f( nx, ny ) );
+				singleLine.push_back( ci::Vec2f( nx + CELL_SIZE + 1, ny ) );
+				break;
+			case EAS:
+				maze[currentX + sideCellAmount * currentY] |= EAS; currentX++;
+				maze[currentX + sideCellAmount * currentY] = WES | WES << 4;
+				singleLine.push_back( ci::Vec2f( nx + CELL_SIZE, ny ) );
+				singleLine.push_back( ci::Vec2f( nx + CELL_SIZE, ny + CELL_SIZE + 1) );
+				break;
+			case SOU:
+				maze[currentX + sideCellAmount * currentY] |= SOU; currentY++;
+				maze[currentX + sideCellAmount * currentY] = NOR | NOR << 4;
+				singleLine.push_back( ci::Vec2f( nx, ny + CELL_SIZE ) );
+				singleLine.push_back( ci::Vec2f( nx + CELL_SIZE + 1, ny + CELL_SIZE ) );
+				break;
+			case WES:
+				maze[currentX + sideCellAmount * currentY] |= WES; currentX--;
+				maze[currentX + sideCellAmount * currentY] = EAS | EAS << 4;
+				singleLine.push_back( ci::Vec2f( nx, ny) );
+				singleLine.push_back( ci::Vec2f( nx, ny + CELL_SIZE + 1 ) );
+		}
+		
+	}
+}
+
+int MazeCreator::getDirection()
+{
+	int d = 1 << rand() % 4;
+	while( true )
+	{
+		for( int x = 0; x < 4; x++ )
+		{
+			if( testDir( d ) ) return d;
+			d <<= 1;
+			if( d > 8 ) d = 1;
+		}
+		d = ( maze[currentX + sideCellAmount * currentY] & 0xf0 ) >> 4;
+		if( !d ) return -1;
+		switch( d )
+		{
+			case NOR: currentY--; break;
+			case EAS: currentX++; break;
+			case SOU: currentY++; break;
+			case WES: currentX--; break;
+		}
+		d = 1 << rand() % 4;
+	}
+}
+
+bool MazeCreator::testDir( int d )
+{
+	switch( d )
+	{
+		case NOR: return ( currentY - 1 > -1 && !maze[currentX + sideCellAmount * ( currentY - 1 )] );
+		case EAS: return ( currentX + 1 < sideCellAmount && !maze[currentX + 1 + sideCellAmount * currentY] );
+		case SOU: return ( currentY + 1 < sideCellAmount && !maze[currentX + sideCellAmount * ( currentY + 1 )] );
+		case WES: return ( currentX - 1 > -1 && !maze[currentX - 1 + sideCellAmount * currentY] );
+	}
+	return false;
+}
--- RecursiveMaze/src/RecursiveMazeApp.cpp
+++ RecursiveMaze/src/RecursiveMazeApp.cpp
@@ -1,6 +1,8 @@
 #include "cinder/app/AppNative.h"
 #include "cinder/gl/gl.h"
 
+#include "MazeCreator.h"
+
 using namespace ci;
 using namespace ci::app;
 using namespace std;
@@ -11,10 +13,16 @@ class RecursiveMazeApp : public AppNative {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+	
+	MazeCreatorRef mc;
+	std::vector<ci::PolyLine2f> maze;
 };
 
 void RecursiveMazeApp::setup()
 {
+	mc = MazeCreatorRef( new MazeCreator( 60 ) );
+	maze = mc->carve();
+	
 }
 
 void RecursiveMazeApp::mouseDown( MouseEvent event )
@@ -28,7 +36,16 @@ void RecursiveMazeApp::update()
 void RecursiveMazeApp::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear( Color( 0, 0, 0 ) );
+	gl::pushMatrices();
+	gl::setMatricesWindow( getWindowSize() );
+	gl::multModelView( Matrix44f::createScale( Vec2i( 1, 1 ) ) );
+	for( auto line = maze.begin(); line != maze.end(); ++line ) {
+		line->setClosed(false);
+		gl::draw(*line);
+	}
+	gl::popMatrices();
+	
 }
 
 CINDER_APP_NATIVE( RecursiveMazeApp, RendererGl )
